05ffd8c3b7ad19728d67605e10640f9155d11cf8
edace91 <eduace91@gmail.com>
2016-10-14 21:50:02 -0500

    fixed the functionsto respond, movie function fully functional

--- liri.js
+++ liri.js
@@ -1,23 +1,8 @@
 // Load the fs package to read and write
-// Load the libraries to grab from 
-// Grab the request package...
 var fs = require('fs');
-var request = require('request');
-var Twitter = require('twitter');
-var spotify = require('spotify');
 
-
-// Import the twitter keys and assign it to a variable
-var twitterKeys = require('./keys');
-var client = new Twitter({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-});
-// Take in action and information to search from user and pass through arguments
+// Take in action command and information to search from user and pass through arguments
 var action = process.argv[2];
-var info = process.argv[3];
 
 //switch between all the Liri commands and the Functions that correspond
 switch(action){
@@ -39,38 +24,54 @@ switch(action){
 //show your last 20 tweets and when they were created
 function tweets(){
 
+	var Twitter = require('twitter');
+
+	// Import the twitter keys and assign it to a variable
+	var twitterKeys = require('./keys');
+	//create the twitter client ID
 	//https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=twitterapi&count=20
+	var client = new Twitter({
+ 		consumer_key: process.env.TWITTER_CONSUMER_KEY,
+ 		consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  		access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  		access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+	});
+
 	var params = {screen_name: 'yeahEd'};
 
-	client.get('statuses/user_timeline.json?screen_name=twitterapi&count=20', params, function(error, tweets, response) {
-  		if(error) throw error;
-    	console.log(tweets);  // The favorites. 
-  		console.log(response);
-  		
+	client.get('statuses/user_timeline.json?screen_name=twitterapi&count=20', params, function(error, tweets, response){
+  		if(error){ console.log(error)
+  		} else {
+  		console.log(JSON.stringify(tweets, null, 2));
+  		console.log(JSON.stringify(response, null, 2));
+  		}
 	});
 }
+
 
 //the Spotify function will output the following:
 //Artist(s), The song's name, preview link, The album.
 function spotify(){
 
-	var nodeSong = info;
+	var spotify = require('spotify');
+
+	var nodeArgs = process.argv;
 
 	var songName = "";
 
-	for (var i=2; i<nodeSong.length; i++){
+	for (var i=3; i<nodeArgs.length; i++){
 
-		if (i>2 && i < nodeSong.length){
+	if (i>3 && i < nodeArgs.length){
 
-			songName = songName + "+" + nodeSong[i];
-		}
-
-		else{
-
-			songName = songName + nodeSong[i];
-		}
-		console.log(songName);
+		songName = songName + " " + nodeArgs[i];
 	}
+
+	else{
+
+		songName = songName + nodeArgs[i];
+	}
+	console.log(songName);
+}
 
 	spotify.search({ type: 'track', query: songName }, function(err, data) {
     	
@@ -78,7 +79,7 @@ function spotify(){
         console.log('Error occurred: ' + err);
         return;
     	}else {
-    		console.log(data);
+    		console.log(JSON.stringify(data, null, 2));
     	}
 	});
 }
@@ -87,33 +88,43 @@ function spotify(){
 // Title, Year, IMDB Rating, Country, Language, 
 // Plot, Actors, Rotten Tomatoes Rating & URL.
 function movie(){
+	var request = require('request');
 
-	var nodeArgs = info;
+	var nodeArgs = process.argv;
 
 	var movieName = "";
 
-	for (var i=2; i<nodeArgs.length; i++){
+	for (var i=3; i<nodeArgs.length; i++){
 
-		if (i>2 && i < nodeArgs.length){
+		if (i>3 && i< nodeArgs.length){
 
-			movieName = movieName + "+" + nodeArgs[i];
+		movieName = movieName + "+" + nodeArgs[i];
+
 		}
 
-		else{
+		else {
 
-			movieName = movieName + nodeArgs[i];
+		movieName = movieName + nodeArgs[i];
 		}
-		console.log(movieName);
+		
 	}
 
-	request('http://www.omdbapi.com/?t='+ movieName + '&y=&plot=short&r=json', function (error, response, body) {
-		// If the request is successful (i.e. if the response status code is 200)
-		console.log(body);
-		if (!error && response.statusCode == 200) {
-		console.log(body);
+	request('http://www.omdbapi.com/?t='+ movieName + '&y=&plot=short&r=json&tomatoes=true', function (error, response, body) {
+    // If the request is successful (i.e. if the response status code is 200)
+	
+	if (!error && response.statusCode == 200) {
+		
 		// Parse the body of the site and recover just the imdbRating
 		// (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it). 
-		console.log( JSON.parse(body)["Year"])
+		console.log( "The movie title: " + JSON.parse(body)["Title"] +
+			"\nThe movie release year: " + JSON.parse(body)["Year"] +
+			"\nThe movie imdb rating: " +JSON.parse(body)["imdbRating"] +
+			"\nThe movie Country of origin: " +JSON.parse(body)["Country"] +
+			"\nThe movie language: " +JSON.parse(body)["Language"] +
+			"\nThe movie plot: " +JSON.parse(body)["Plot"] +
+			"\nThe movie actors: " +JSON.parse(body)["Actors"] +
+			"\nThe movie Rotten Tomatoes score: " +JSON.parse(body)["tomatoMeter"] +
+			"\nThe movie Rotten Tomatoes url: " +JSON.parse(body)["tomatoURL"]);
 		}
 	});
 }
